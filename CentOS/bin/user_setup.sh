3ceee1c577dcfa2ab83888c31fa22c353cccdb10
sfanjoy <sfanjoy>
2024-04-27 11:58:29 -0400

    authkey

--- CentOS/bin/user_setup.sh
+++ CentOS/bin/user_setup.sh
@@ -14,12 +14,20 @@ else
   /usr/sbin/useradd -s /bin/bash sfanjoy
   echo "Setting sfanjoy password..."
   passwd sfanjoy
-/usr/sbin/usermod -a -G ssh-uzer sfanjoy
+  /usr/sbin/usermod -a -G ssh-uzer sfanjoy
 fi
 mkdir -p /home/sfanjoy/bin
 mkdir -p /home/sfanjoy/rpms
 cp ../home/sfanjoy/vimrc /home/sfanjoy/.vimrc
 cp ../home/sfanjoy/bashrc /home/sfanjoy/.bashrc
+echo "Installing keys for User $2..."
+mkdir -p -m 700 /home/sfanjoy/.ssh
 cp ../home/sfanjoy/id_ed25519.pub /home/sfanjoy/.ssh/authorized_keys
+chmod 700 /home/sfanjoy/.ssh
+chmod 600 /home/sfanjoy/.ssh/*
+chmod 711 /home/sfanjoy/.ssh/authorized_keys
+chmod 644 /home/sfanjoy/.ssh/*.pub
+chown -R sfanjoy:sfanjoy /home/$2/.ssh
+echo "User sfanjoy keys installed"
 chown -R sfanjoy:sfanjoy /home/sfanjoy
 echo "User sfanjoy Configured"
