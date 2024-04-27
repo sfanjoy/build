27f472c2da37d7f9aaac3d249c929ddbfcbeaa54
sfanjoy <sfanjoy>
2024-04-27 11:42:02 -0400

    authkey

--- CentOS/bin/user_setup.sh
+++ CentOS/bin/user_setup.sh
@@ -9,14 +9,17 @@ uinfo=`getent passwd sfanjoy`
 #
 if [[ ${#uinfo} -gt 0 ]]; then
   echo "User sfanjoy already exists...Good"
-  exit
-fi
-echo "Adding User sfanjoy..."
-/usr/sbin/useradd -s /bin/bash sfanjoy
-echo "Setting sfanjoy password..."
-passwd sfanjoy
+else
+  echo "Adding User sfanjoy..."
+  /usr/sbin/useradd -s /bin/bash sfanjoy
+  echo "Setting sfanjoy password..."
+  passwd sfanjoy
 /usr/sbin/usermod -a -G ssh-uzer sfanjoy
+fi
 mkdir -p /home/sfanjoy/bin
 mkdir -p /home/sfanjoy/rpms
+cp ../home/sfanjoy/vimrc /home/sfanjoy/.vimrc
+cp ../home/sfanjoy/bashrc /home/sfanjoy/.bashrc
+cp ../home/sfanjoy/id_ed25519.pub /home/sfanjoy/.ssh/authorized_keys
 chown -R sfanjoy:sfanjoy /home/sfanjoy
 echo "User sfanjoy Configured"
